02e27249800daf6859bc8ad52b2b541648f3a061
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-12-20 19:24:06 +0200

    adding

--- src/components/Pages/Carspage.js
+++ src/components/Pages/Carspage.js
@@ -2,9 +2,21 @@ import './categories.css'
 import store from '../Store/Products';
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 const Car = (props) => {
+  const Cars = [
+    { id:1, name: 'Car stearins', image: 'https://image.made-in-china.com/202f0j00uWpgQrUdVibI/Hot-Selling-Car-Steering-Wheel-Cover-Car-Accessories-Car-Decoration-Auto-Spare-Part.webp', description: ' Hot Selling Car Steering Wheel Cover Car Accessories Car Decoration Auto Spare Part' },
+    { id:2, name: 'Chair xenon', image: 'https://txonstores.com/content/images/thumbs/0003459_seat-protectors_300.jpeg', description: 'Car seat protector for front seats  Non-skid mesh backing keeps car seat protectors in place  Adjustable belts secure protector to most all makes and model'
+ },
+    { id:3, name: 'phone holder', image: 'https://www.techadvisor.com/cmsdata/slideshow/3663427/best-car-tech-accessories-2020_thumb1200_16-9.jpg', description: 'This is one of the more expensive accessories you can buy for your car. ' },
+    { id:4, name: 'duck duck ', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj_B5SzWdQM9qBo0-yS0w-RisGoEjihv2Kaw&usqp=CAU', description: 'We provide free worldwide shipping on all orders. Please allow 1 – 2 business days for processing and 3 – 5 days for delivery.' },
+    { id:5, name: 'For auto vent Dusting ', image: 'https://img.buzzfeed.com/buzzfeed-static/static/2020-08/10/14/asset/b583a57efe7f/sub-buzz-6898-1597069393-40.jpg?output-quality=auto', description: 'A container of gel cleaning putty that you can use to peel the dirt off your dashboard, air vents, and pretty much everything else you own. It feels like slime, so its super fun to use, too.' },
 
+  ]
   useEffect(() => {
 
     store.dispatch({
@@ -13,7 +25,7 @@ const Car = (props) => {
     });
     store.dispatch({
       type: 'SET_PRODUCTS',
-      payload: ['Porsche', 'Hyundai', 'SAIC ', 'BMW']
+      payload: Cars
     })
 
   }, [])
@@ -21,30 +33,41 @@ const Car = (props) => {
   console.log(props.products);
   return (
     <>
+    <div className="home">
 
-      <div className="home">
+      <h1>this is the active category 👉 {props.categories.activeCategory}</h1>
+    </div>
+    <div className="center">
 
-        <h1>this is the active category 👉 {props.categories.activeCategory}</h1>
-
-      </div>
-      <div className="home">
-        <br></br>
-        <h1>This is the Products </h1>
-        <ul></ul>
+      <Row xs={3} md={4} >
         {props.products.map(element => {
-
           return (
             <>
+            <Col>
+              <Card style={{ width: '18rem' }} bg='Dark'>
 
-              <li key="{element}">{element}</li>
+                <Card.Img variant="top" src={element.image} style={{ height: '160px', cover: 'fill' }} />
+                <Card.Body>
+                  <Card.Title>{element.name}</Card.Title>
+                  <Card.Text>
+                    {element.description}
+                  </Card.Text>
+                  <Button variant="outline-success">Add to Cart</Button>{' '}
+                </Card.Body>
+              </Card>
+              </Col>
             </>
           )
         })}
+      </Row>
 
       </div>
 
-    </>
-  );
+
+
+  </>
+);
+
 
 
 };
--- src/components/Pages/Jewelry.js
+++ src/components/Pages/Jewelry.js
@@ -3,8 +3,20 @@ import './categories.css'
 import store from '../Store/Products';
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 const Jewelry = (props) => {
+  const jewelry = [
+    { id: 1, name: 'Allison Kaufman', image: 'https://imgs-s1.jewelryimages.net/vendor-jewelry-images/galleries/allisonkaufman/1x1/F213-70447-W.jpg?v=7', description: 'Allison-Kaufman Company, in business since 1920,diamond jewelry.', price: '20$' },
+    { id: 2, name: 'Diamond Bracelets', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2OI3qFITYzcQGzXQUxMC8q1VfDfian1m1TA&usqp=CAU ', description: '', price: '20$' },
+    { id: 3, name: '14KT Gold Bracelet', image: 'https://imgs-s1.jewelryimages.net/vendor-jewelry-images/galleries/allisonkaufman/1x1/H300-04074-YW.jpg?v=7', description: '', price: '20$' },
+    { id: 4, name: '14KT Gold Bracelet', image: 'https://imgs-s1.jewelryimages.net/vendor-jewelry-images/galleries/allisonkaufman/1x1/M210-99474-W.jpg?v=7', description: '', price: '20$' },
+    { id: 5, name: 'Ladies Bag Karina By Animo Italia - Tote Bag', image: 'https://imgs-s1.jewelryimages.net/vendor-jewelry-images/galleries/allisonkaufman/1x1/G217-34056-Y.jpg?v=7', description: '', price: '20$' },
+
+  ]
   useEffect(() => {
     store.dispatch({
       type: 'SELECT_CATEGORY',
@@ -12,7 +24,7 @@ const Jewelry = (props) => {
     });
     store.dispatch({
       type: 'SET_PRODUCTS',
-      payload: ['Jewelry1', 'Jewelry2', 'Jewelry3 ', 'Jewelry4']
+      payload: jewelry
     })
   }, [])
   console.log(props.products);
@@ -21,21 +33,34 @@ const Jewelry = (props) => {
       <div className="home">
 
         <h1>this is the active category 👉 {props.categories.activeCategory}</h1>
-
       </div>
-      <div className="home">
-        <br></br>
-        <h1>This is the Products </h1>
-        <ul></ul>
-        {props.products.map(element => {
+      <div className="center">
 
-          return (
-            <>
+        <Row xs={3} md={4} >
+          {props.products.map(element => {
+            return (
+              <>
+                <Col>
+                  <Card style={{ width: '18rem' }} bg='Dark'>
 
-              <li key="{element}">{element}</li>
-            </>
-          )
-        })}
+                    <Card.Img variant="top" src={element.image} style={{ height: '160px', cover: 'fill' }} />
+                    <Card.Body>
+                      <Card.Title>{element.name}</Card.Title>
+                      <Card.Text>
+                        {element.description}
+                      </Card.Text>
+                      <ListGroup variant="flush">
+                        <ListGroup.Item>{element.price}</ListGroup.Item>
+
+                      </ListGroup>
+                      <Button variant="outline-success">Add to Cart</Button>{' '}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </>
+            )
+          })}
+        </Row>
 
       </div>
     </>
